bec2b0cbd51fc19b83f7195102f16e370d861d55
Tim Beck <tmbeck@gmail.com>
2026-05-12 19:59:23 -0700

    Cherry-pick 880be04: vendor Borland TASM/TLINK + dosbox-build make.bat
    Cherry-picked from yetmorecode/dos32a-ng@880be04 ("Added bin").
    
    Vendors the Borland 16-bit DOS toolchain (TASM32 + TLINK + DPMI runtime
    files) under bin/ and switches src/dos32a/make.bat from Watcom wcl to
    TLINK, with relative paths to bin/. This is the entry point the
    containerized DOSBox build will run unmodified inside the emulator.
    
    Existing src/dos32a/make.bat (which used wcl) replaced with the
    dos32a-ng version, since the whole pivot is to use TASM/TLINK + DOSBox
    instead of OW v2 + native wasm.

--- src/dos32a/make.bat
+++ src/dos32a/make.bat
@@ -1,13 +1,13 @@
 @echo off
-echo Creating DOS/32 Advanced DOS Extender
-echo *************************************
-echo.
+@echo Creating DOS/32 Advanced DOS Extender (2021 next-gen)
+@echo *****************************************************
+@echo.
 
-%dos32a%\src\sutils\build\build TEXT\oemtitle.asm 1958
-tasm32 -dEXEC_TYPE=0 %TASMFLAGS% -c kernel.asm
-tasm32 -dEXEC_TYPE=0 %TASMFLAGS% -c dos32a.asm
-wcl %WCLFLAGS% -lr -fm=dos32a -fe=dos32a dos32a.obj kernel.obj
-rem copy dos32a.exe %WATCOM%\BINW
-copy dos32a.exe %DOS32A%\binw >nul
 del *.obj
+del *.lst
 del *.exe
+
+..\..\bin\tasm32 /dEXEC_TYPE=0 /m /ml /c /la kernel.asm
+..\..\bin\tasm32 /dEXEC_TYPE=0 /m /ml /c /la dos32a.asm
+
+..\..\bin\tlink /3 dos32a kernel,dos32a-ng.exe
